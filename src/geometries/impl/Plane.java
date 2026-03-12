9a6b31ca0d9d5019180d1ca5b9205f79f9ce53a6
moshe <mozs770.hanau@gmail.com>
2026-03-12 23:47:02 +0200

    make some changes

--- src/geometries/impl/Plane.java
+++ src/geometries/impl/Plane.java
@@ -5,22 +5,25 @@ import primitives.Vector;
 
 public class Plane {
 
-    // is the field need to be private and final?
-    private Point _point;
-    private Vector _vector;
+    private final Point _point;
+    private Vector _normal;
+
     Plane(Point p1 , Point p2 , Point p3) {
+        this._point = p1;
+        /*
             Vector vector1 = p1.subtract(p2);
             Vector vector2 = p2.subtract(p3);
-            _vector = vector1.crossProduct(vector2);
+            _normal= vector1.crossProduct(vector2);
             _point = p1;
+         */
     }
 
     Plane(Vector normal , Point point) {
-        _vector = normal.normalize();
+        _normal = normal.normalize();
         _point = point;
     }
 
     public Vector getNormal() {
-        return _vector;
+        return _normal;
     }
 }
