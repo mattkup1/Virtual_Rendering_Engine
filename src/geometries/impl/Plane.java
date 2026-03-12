07bfe80c1769af8720fae96d428b4004f6f8be68
mattkup1 <104730300+mattkup1@users.noreply.github.com>
2026-03-12 17:54:54 -0400

    added javadoc and todo to Plane.java

--- src/geometries/impl/Plane.java
+++ src/geometries/impl/Plane.java
@@ -3,27 +3,69 @@ package geometries.impl;
 import primitives.Point;
 import primitives.Vector;
 
+/**
+ * Represents a plane in 3D Cartesian coordinate system.
+ * A plane is defined by a point on the plane and a normal vector perpendicular to it.
+ *
+ * @author mattkuperwasser
+ * @author moshehanau
+ */
 public class Plane {
 
+    /** A point on the plane */
     private final Point _point;
-    private Vector _normal;
 
-    Plane(Point p1 , Point p2 , Point p3) {
+    /** The normal vector to the plane */
+    private final Vector _normal;
+
+    /**
+     * Constructs a plane from three points on the plane.
+     * The points must be unique and not collinear.
+     *
+     * @param p1 first point
+     * @param p2 second point
+     * @param p3 third point
+     * @throws IllegalArgumentException if points are collinear or identical
+     */
+    public Plane(Point p1, Point p2, Point p3) {
         this._point = p1;
+
         /*
-            Vector vector1 = p1.subtract(p2);
-            Vector vector2 = p2.subtract(p3);
-            _normal= vector1.crossProduct(vector2);
-            _point = p1;
+        // TODO: check that all 3 points are not on the same straight
+        Vector v1 = p2.subtract(p1);
+        Vector v2 = p3.subtract(p1);
+
+        // The normal is the cross product of two vectors on the plane
+        this._normal = v1.crossProduct(v2).normalize();
          */
     }
 
-    Plane(Vector normal , Point point) {
-        _normal = normal.normalize();
-        _point = point;
+    /**
+     * Constructs a plane from a normal vector and a point on the plane.
+     *
+     * @param normal the normal vector to the plane
+     * @param point  a point on the plane
+     */
+    public Plane(Vector normal, Point point) {
+        this._normal = normal.normalize();
+        this._point = point;
     }
 
+    /**
+     * Returns the normal vector of the plane.
+     *
+     * @return the normal vector
+     */
     public Vector getNormal() {
         return _normal;
+    }
+
+    /**
+     * Returns the point defining the plane.
+     *
+     * @return the point
+     */
+    public Point getPoint() {
+        return _point;
     }
 }
