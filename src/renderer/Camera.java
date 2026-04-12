e84911d397cbb9e7b44356755eff57526d188ee8
mattkup1 <104730300+mattkup1@users.noreply.github.com>
2026-04-12 16:05:58 +0300

    added some basic fields to the camera class

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -13,19 +13,51 @@ public class Camera {
     /**
      * {@link Point} representing the camera location
      */
-    private final Point p0;
+    private final Point _p0;
     /**
      * {@link Vector} representing the direction in which the camera is pointing
      */
-    private final Vector vTo;
+    private final Vector _vTo;
     /**
      * {@link Vector} representing the upward direction relative to the camera
      */
-    private final Vector vUp;
+    private final Vector _vUp;
     /**
      * {@link Vector} representing the right hand side direction relative to the camera
      */
-    private final Vector vRight;
+    private final Vector _vRight;
+    /**
+     * View plane width
+     */
+    private final double _width;
+    /**
+     * View plane height
+     */
+    private final double _height;
+    /**
+     * Distance from the camera to the view plane
+     */
+    private final double _distance;
+    /**
+     * Number of pixel columns in the view plane
+     */
+    private final int _nX;
+    /**
+     * Number of pixel rows in the view plane
+     */
+    private final int _nY;
+    /**
+     * {@link Point} representing the center of the view plane
+     */
+    private final Point _vpCenter;
+    /**
+     * Individual pixel width
+     */
+    private final double pixelWidth;
+    /**
+     * Individual pixel height
+     */
+    private final double pixelHeight;
 
     private Camera() {
     }
