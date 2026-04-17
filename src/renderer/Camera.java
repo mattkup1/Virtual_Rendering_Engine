0cbd6a1c76e4bbbfd723d8b659d2cf7452cb7445
mattkup1 <104730300+mattkup1@users.noreply.github.com>
2026-04-17 14:41:25 +0300

    continued implementing the camera class

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -1,10 +1,9 @@
 package renderer;
 
+import java.util.MissingResourceException;
 import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
-
-import static primitives.Util.isZero;
 
 /**
  * Camera class representing a camera in a 3D space
@@ -56,16 +55,17 @@ public class Camera implements Cloneable {
     /**
      * Individual pixel width
      */
-    private double pixelWidth;
+    private double _pixelWidth;
     /**
      * Individual pixel height
      */
-    private double pixelHeight;
+    private double _pixelHeight;
 
     /**
      * Empty camera constructor
      */
-    private Camera() {}
+    private Camera() {
+    }
 
     /**
      * Camera builder getter
@@ -88,6 +88,10 @@ public class Camera implements Cloneable {
          * Initialize camera object
          */
         private final Camera _camera = new Camera();
+        /**
+         * A {@link Point} which the camera is facing
+         */
+        private Point _pTarget;
 
         /**
          * Set the camera location
@@ -109,30 +113,33 @@ public class Camera implements Cloneable {
          * @throws IllegalArgumentException if the vectors are not orthogonal
          */
         public Builder setDirection(Vector vTo, Vector vUp) {
-            if (!isZero(vTo.dotProduct(vUp)))
-                throw new IllegalArgumentException("vTo and vUp must be orthogonal");
+//            if (!isZero(vTo.dotProduct(vUp)))
+//                throw new IllegalArgumentException("vTo and vUp must be orthogonal");
 
-            _camera._vTo = vTo;
-            _camera._vUp = vUp;
+            _camera._vTo = vTo.normalize();
+            _camera._vUp = vUp.normalize();
+            this._pTarget = null;
 
             return this;
         }
 
         /**
-         * Initializes the camera direction vectors based on a a given target point and up vector
+         * Initializes the camera direction vectors based on a given target point and up vector
          *
          * @param pTarget the target point at which the camera is pointing
          * @param vUp     the upward direction vector relative to the camera
          * @return the builder object
          */
         public Builder setDirection(Point pTarget, Vector vUp) {
-            final Vector vTo = pTarget.subtract(_camera._p0);
-            if (!isZero(vTo.dotProduct(vUp)))
-                throw new IllegalArgumentException("vTo and vUp must be orthogonal");
+            this._pTarget = pTarget;
+//            final Vector vTo = pTarget.subtract(_camera._p0);
+//            if (!isZero(vTo.dotProduct(vUp)))
+//                throw new IllegalArgumentException("vTo and vUp must be orthogonal");
 
-            _camera._vTo = vTo.normalize();
+//            _camera._vTo = vTo.normalize();
             _camera._vUp = vUp.normalize();
-            _camera._vRight = _camera._vUp.crossProduct(_camera._vTo).normalize();
+            this._pTarget = null;
+//            _camera._vRight = _camera._vUp.crossProduct(_camera._vTo).normalize();
 
             return this;
         }
@@ -146,14 +153,17 @@ public class Camera implements Cloneable {
          * @throws IllegalArgumentException if the camera direction and the y axis are not orthogonal
          */
         public Builder setDirection(Point p) {
-            final Vector vTo = p.subtract(_camera._p0);
-            final Vector vUp = Vector.AXIS_Y
-            if (!isZero(vTo.dotProduct(vUp)))
-                throw new IllegalArgumentException("Camera direction and the y axis must be orthogonal");
+//            final Vector vTo = p.subtract(_camera._p0);
+            this._pTarget = p;
+            final Vector vUp = Vector.AXIS_Y;
 
-            _camera._vTo = vTo.normalize();
-            _camera._vUp = vUp;
-            _camera._vRight = vTo.crossProduct(vUp).normalize();
+            return this;
+//            if (!isZero(vTo.dotProduct(vUp)))
+//                throw new IllegalArgumentException("Camera direction and the y axis must be orthogonal");
+
+//            _camera._vTo = vTo.normalize();
+//            _camera._vUp = vUp;
+//            _camera._vRight = vTo.crossProduct(vUp).normalize();
         }
 
         /**
@@ -163,39 +173,118 @@ public class Camera implements Cloneable {
          * @return the builder objects
          */
         public Builder setVpDistance(double distance) {
-            return null;
+            _camera._distance = distance;
+            return this;
         }
 
-        public Builder setVpSize(double d1, double d2) {
-            return null;
+        /**
+         * Set the measurements of the view plane
+         *
+         * @param width  the view plane width
+         * @param height the view plane height
+         * @return the builder object
+         */
+        public Builder setVpSize(double width, double height) {
+            _camera._width = width;
+            _camera._height = height;
+
+            return this;
         }
 
-        public Builder setResolution(int i1, int i2) {
-            return null;
+        /**
+         * Set the resolution (number of pixels in the view plane)
+         *
+         * @param nX number of pixel columns
+         * @param nY number of pixel rows
+         * @return the builder object
+         */
+        public Builder setResolution(int nX, int nY) {
+            _camera._nX = nX;
+            _camera._nY = nY;
+
+            return this;
         }
 
-        private Builder calcVectors() {
-            return null;
+        /**
+         * Helper function to calculate the camera direction vectors
+         *
+         */
+        private void calcVectors() {
+            if (_camera._vTo == null)
+                _camera._vTo = this._pTarget.subtract(_camera._p0).normalize();
+
+            _camera._vRight = _camera._vUp.crossProduct(_camera._vTo).normalize();
+            _camera._vUp = _camera._vTo.crossProduct(_camera._vRight).normalize();
         }
 
-        private Builder calcVpCenter() {
-            return null;
+        private void calcVpCenter() {
+            _camera._vpCenter = _camera._p0.add(_camera._vTo.scale(_camera._distance));
+
         }
 
-        private Builder checkAndSetResolution() {
-            return null;
+        /**
+         * Validate number of pixels in the rows and columns of the view plane
+         */
+        private void checkResolution() {
+            if (_camera._nX <= 0 || _camera._nY <= 0)
+                throw new IllegalArgumentException("Number of pixels must be positive");
         }
 
-        private Builder checkAndSetOrientation() {
-            return null;
+        /**
+         * Validate the location and direction of the camera
+         *
+         * @throws MissingResourceException if p0 is missing
+         * @throws IllegalArgumentException if vTo is parallel to vUp
+         */
+        private void checkLocationAndDirection() {
+            if (_camera._p0 == null)
+                throw new MissingResourceException("Camera location must be initialized", "Camera", "p0");
+
+            if (_camera._vUp == null)
+                throw new MissingResourceException("Camera vUp vector must be initialized", "Camera", "vUp");
+
+            if (_camera._vTo == null && this._pTarget == null)
+                throw new MissingResourceException(
+                        "Camera vTo vector or pTarget point must be initialized", "Camera", "vTo or pTarget");
+
+            calcVectors();
         }
 
-        private Builder checkAndSetViewPlane() {
-            return null;
+        /**
+         *
+         *
+         */
+        private void checkViewPlane() {
+            if (_camera._width < 0)
+                throw new IllegalArgumentException("view plane must be positive");
+            if (_camera._height < 0)
+                throw new IllegalArgumentException("view plane height must be positive");
+            if (_camera._distance < 0)
+                throw new IllegalArgumentException("distance must be positive");
+
+            calcVpCenter();
+            _camera._pixelWidth = _camera._width / _camera._nX;
+            _camera._pixelHeight = _camera._height / _camera._nY;
         }
 
         public Camera build() {
-            return _camera;
+            checkResolution();
+            checkLocationAndDirection();
+            checkViewPlane();
+            try {
+                return (Camera) _camera.clone();
+            } catch (CloneNotSupportedException _) {
+                return null;
+            }
         }
     }
 }
+
+
+//Camera camera = Camera.getBuilder()
+//        .setLocation(...)
+//        .setDirection(...)
+//    .setVpSize(...)
+//    .setVpDistance(...)
+//    .setResolution(...)
+//    .build();
