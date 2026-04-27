d8d39ac81be6a0659ee928c57685d349892c3412
mattkup1 <104730300+mattkup1@users.noreply.github.com>
2026-04-27 17:04:03 +0300

    added and implemented SimpleRayTracer class

---
+++ src/renderer/SimpleRayTracer.java
@@ -0,0 +1,57 @@
+package renderer;
+
+import primitives.Color;
+import primitives.Point;
+import primitives.Ray;
+import scene.Scene;
+
+/**
+ * A basic implementation of a ray tracer that performs simple ray-geometry
+ * intersection tests and returns basic color results.
+ * <p>
+ * In this initial version, the tracer identifies the closest intersection
+ * point and colors it using the scene's ambient light intensity, without
+ * accounting for direct light sources, shadows, or reflections.
+ * </p>
+ * * @author mattkuperwasser
+ *
+ * @author moshehanau
+ */
+class SimpleRayTracer extends RayTracerBase {
+
+    /**
+     * Constructs a SimpleRayTracer with a given scene.
+     *
+     * @param scene the scene to be rendered
+     */
+    public SimpleRayTracer(Scene scene) {
+        super(scene);
+    }
+
+    /**
+     * Traces a ray into the scene and calculates the color of the closest
+     * intersection point found.
+     *
+     * @param ray the ray to trace
+     * @return the color at the closest intersection point, or the scene's
+     * background color if no intersections occur.
+     */
+    @Override
+    public Color traceRay(Ray ray) {
+        var intersections = this._scene._geometries.findIntersections(ray);
+        if (intersections == null)
+            return this._scene._background;
+
+        return calcColor(ray.findClosestPoint(intersections));
+    }
+
+    /**
+     * Calculates the color at a specific intersection point.
+     *
+     * @param intersection the point on a geometry surface
+     * @return the calculated color intensity
+     */
+    private Color calcColor(Point intersection) {
+        return this._scene._ambientLight.getIntensity();
+    }
+}
