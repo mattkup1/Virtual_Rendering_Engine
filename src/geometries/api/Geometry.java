d117c69ebdca36c20f07761f56caae2fab7bd8e8
mattkup1 <104730300+mattkup1@users.noreply.github.com>
2026-05-08 11:08:21 +0300

    added Intersection PDS to Intersectable class

--- src/geometries/api/Geometry.java
+++ src/geometries/api/Geometry.java
@@ -1,5 +1,6 @@
 package geometries.api;
 
+import primitives.Color;
 import primitives.Point;
 import primitives.Vector;
 
@@ -13,7 +14,32 @@ public abstract class Geometry extends Intersectable {
     /**
      * Default constructor to satisfy JavaDoc generator and other problems in children classes
      */
-    public Geometry() { /* To satisfy JavaDoc generator */ }
+    public Geometry() { /* To satisfy Javadoc generator */ }
+
+    /**
+     * The geometry's emission color
+     */
+    private Color _emission = Color.BLACK;
+
+    /**
+     * Gets the geometry's emission color
+     *
+     * @return the geometry's emission color
+     */
+    public Color getEmission() {
+        return _emission;
+    }
+
+    /**
+     * Sets the geometry's emission color and returns the geometry to allow chained method calls
+     *
+     * @param emission the emission color
+     * @return the geometry
+     */
+    public Geometry setEmission(Color emission) {
+        _emission = emission;
+        return this;
+    }
 
     /**
      * Abstract method to get the geometry's Normal vector
--- src/geometries/api/Intersectable.java
+++ src/geometries/api/Intersectable.java
@@ -24,4 +24,43 @@ public abstract class Intersectable {
      * @return the list of intersection points between the ray and the geometric shape
      */
     public abstract List<Point> findIntersections(Ray ray);
+
+    /**
+     * Passive Data Structure (PDS) representing an intersection point between a ray and a geometry
+     * An intersection consists of the point in space as well as the physical geometry
+     */
+    public static final class Intersection {
+        /**
+         * The geometry
+         */
+        public final Geometry geometry;
+        /**
+         * The intersection point
+         */
+        public final Point intersection;
+
+        /**
+         * Constructs an Intersection object
+         *
+         * @param geometry     the geometry
+         * @param intersection the intersection point
+         */
+        public Intersection(Geometry geometry, Point intersection) {
+            this.geometry = geometry;
+            this.intersection = intersection;
+        }
+
+        @Override
+        public String toString() {
+            return "Intersection: " + geometry.toString() + " " + intersection.toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || this.getClass() != o.getClass()) return false;
+            Intersection other = (Intersection) o;
+            return this.geometry.equals(other.geometry) && this.intersection.equals(other.intersection);
+        }
+    }
 }
